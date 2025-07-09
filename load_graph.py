2d11fd4aea69d5338c833fb7842ccc0af24ddecd
bryanluna0 <159389778+bryanluna0@users.noreply.github.com>
2025-07-09 11:18:49 -0400

    Add graph name

--- load_graph.py
+++ load_graph.py
@@ -3,9 +3,10 @@ import os
 
 class DiGraph:
     def __init__(self, graph_file):
+        self.name = graph_file[0:-4]
         self.G = nx.read_gml(os.path.join("input_graphs", graph_file)) 
+        self.G.graph['name'] = self.name
         n_edges = len(self.G.edges())
         weight = {e: 1.0 for e in self.G.edges()}
         nx.set_edge_attributes(self.G, weight, 'weight')
         print("Data loaded. \nNumber of nodes： {}\nNumber of edges: {}".format(self.G.number_of_nodes(), self.G.number_of_edges()))
-        
